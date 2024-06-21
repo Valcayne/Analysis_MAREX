28434e1fbd4d279e6ed90814e91136da62fb7e06
Valcayne <valcayne@gmail.com>
2024-06-21 14:53:26 +0200

    test

--- Make2DHistos/Condor/ProgramsLaunch/LaunchCondorMergeMake2D.sh
+++ Make2DHistos/Condor/ProgramsLaunch/LaunchCondorMergeMake2D.sh
@@ -1,7 +1,7 @@
 # Simple HTCondor submit description file
 # Everything with a leading # is a comment
  
-executable   = /afs/cern.ch/user/v/valcayne/Workspace/2024_Er_U_ProgramsC6D6/Analysis_nTOF_v01/Make2DHistos/Merge2DHisto
+executable   = /afs/cern.ch/user/v/valcayne/Workspace/2024_Er_U_ProgramsC6D6/Analysis_nTOF_v01/Make2DHistos/Merge2DHistos
 arguments    = $(run)
 #output       = outputs_condor/DTAnalyzer01_$(EXTRRun)_$(RAWRun)_det$(DetN)_$(Quant)_$(nEn)_$(nE1).out
 output       = Condor/outputs/output_$(run).out
