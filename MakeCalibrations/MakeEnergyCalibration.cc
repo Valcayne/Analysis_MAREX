965f6d6a43eff417313b48e8210036ce28eaa456
Valcayne <valcayne@gmail.com>
2024-07-02 10:10:22 +0200

    test

--- MakeCalibrations/MakeEnergyCalibration.cc
+++ MakeCalibrations/MakeEnergyCalibration.cc
@@ -566,9 +566,9 @@ void FitEnergy(string outfolder, int detn) {
 
   TF1* myfitpol3 = new TF1("myfitpol3", FunPol2andPol1, 0.1, 30000, 4);  // pol3
   myfitpol3->SetParLimits(0, 0.001, 0.01);
-  myfitpol3->SetParLimits(1, p1pol2, p1pol2);
+  myfitpol3->SetParLimits(1, p1pol2 * 0.99, p1pol2 * 1.01);
   // myfitpol3->SetParLimits(2, 3000, 10000);
-  myfitpol3->SetParLimits(3, 3000, 10000);
+  myfitpol3->SetParLimits(3, 2000, 10000);
 
   gr->Fit("myfitpol3", "R0");
   myfitpol3->Draw("same");
