ee554b6b57a4d55ed9daf529f0707c27a421f083
Valcayne <valcayne@gmail.com>
2024-07-04 16:23:22 +0200

    test

--- Make2DHistos/Condor/ProgramsLaunch/LaunchCondorMake2DGainCorrected.sh
+++ Make2DHistos/Condor/ProgramsLaunch/LaunchCondorMake2DGainCorrected.sh
@@ -2,7 +2,7 @@
 # Everything with a leading # is a comment
  
 executable   = /afs/cern.ch/user/v/valcayne/Workspace/2024_Er_U_ProgramsC6D6/Analysis_nTOF_v01/Make2DHistos/Make2DHistos
-arguments    = $(run) ${GainCalibrated}
+arguments    = $(run) $(GainCalibrated)
 #output       = outputs_condor/DTAnalyzer01_$(EXTRRun)_$(RAWRun)_det$(DetN)_$(Quant)_$(nEn)_$(nE1).out
 output       = Condor/outputs/output_$(run).out
 error        = Condor/outputs/error_$(run).err
